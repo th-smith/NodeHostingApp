df401304eb37f6c22b4a0390b532626abc720466
Tommy Smith <tommy.smith@live.no>
2023-09-10 21:55:37 +0200

    Changed where the green node software is dowloaded from.

--- script/install-green-node.sh
+++ script/install-green-node.sh
@@ -29,37 +29,49 @@ fi
 
 echo "Installing $brand-node-$username.service (wait $((sleepdelay))s ..)"
 
-#Create node credential file
-cat >"/home/$username/.$brand-node.yaml" <<EOL
-username: '$email'
-password: '$password'
-nodename: '$brand-node-$nodeindex'
+#Create config expect file
+expectfile="/home/$username/.$brand-node.exp"
+cat >"$expectfile" <<EOL
+#!/usr/bin/expect
+spawn /usr/local/bin/$brand-node-$username config
+expect "Green Email:"
+send "$email\r"
+expect "Green Password:"
+send "$password\r"
+expect eof
 EOL
-chown $username /home/$username/.$brand-node.yaml
+chown $username /home/$username/.$brand-node.exp
+chmod +x "$expectfile"
 
 #Download locations
-domain="static.connectblockchain.net"
-download_url="https://$domain/softnode/$brand-node_linux-amd64"
+date=$(date +%s)
+domain="download.nerdunited.net"
+download_url="https://$domain/node-binaries/$brand/prod/${brand}_linux-amd64?$date"
 node="/usr/local/bin/$brand-node-$username"
 
 #Remove old software
 rm -f $node
-
 #Download node software
 wget --continue "$download_url" --output-document "$node" --quiet
 chmod +x "$node"
-#$node config
+
+#Configure and remove input file after use.
+sudo -u $username $expectfile
+rm $expectfile
 
 #Create service
 cat >"/etc/systemd/system/$brand-node-$username.service" <<EOL
 [Unit]
 Description=$brand node
 After=network.target
+StartLimitIntervalSec=0
 [Service]
 User=$username
 ExecStart=$node
-ExecStartPre=/bin/sleep $sleepdelay
+StartLimitInterval=0
 Restart=always
+RestartSec=30
+Environment="NODE_LOG_LEVEL=info"
 [Install]
 WantedBy=multi-user.target
 EOL
